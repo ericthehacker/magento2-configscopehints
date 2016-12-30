fb8faf1aa4b327238b17ecc838776e3ee1ee42cc
Eric Wiese <eric.wiese@classyllama.com>
2016-12-30 11:25:25 -0600

    Improve value hint styling

--- view/adminhtml/web/js/configscopehints.js
+++ view/adminhtml/web/js/configscopehints.js
@@ -3,7 +3,7 @@ require([
 ], function ($) {
     $(document).ready(function() {
         $('.overridden-hint-list').on('click', '.override-scope', function() {
-            $(this).next('.override-value').toggleClass('visible');
+            $(this).toggleClass('open').next('.override-value').toggleClass('visible');
         });
     });
 });
