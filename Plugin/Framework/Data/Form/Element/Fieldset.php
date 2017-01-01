be949658bacdcd21e17a9bd4ebe0750514f80757
Eric Wiese <eric.wiese@classyllama.com>
2017-01-01 09:23:08 -0600

    POC support for elements with different config path than ID

--- Plugin/Framework/Data/Form/Element/Fieldset.php
+++ Plugin/Framework/Data/Form/Element/Fieldset.php
@@ -6,6 +6,8 @@ use \Magento\Framework\Data\Form\Element\Fieldset as OriginalFieldset;
 
 class Fieldset
 {
+    const CONFIG_PATH_NAME_PATTERN = '#\[.+\]#U';
+
     /**
      * @var \EW\ConfigScopeHints\Helper\Data
      */
@@ -30,6 +32,28 @@ class Fieldset
     }
 
     /**
+     * Attempt to get config path from element HTML name
+     *
+     * @param string $name
+     * @return null|string
+     */
+    protected function getConfigPathFromName($name) {
+        //$name is of form groups[some_group_name][fields][target_config_path_value][value]
+        if(preg_match_all(self::CONFIG_PATH_NAME_PATTERN, $name, $nameComponents)) {
+            $nameComponents = $nameComponents[0];
+            foreach($nameComponents as $i => $nameComponent) {
+                if($nameComponent != '[fields]') continue;
+
+                if(!isset($nameComponents[$i+1])) break;
+
+                return trim($nameComponents[$i+1], '[]');
+            }
+        }
+
+        return null;
+    }
+
+    /**
      * If field is overwritten at more specific scope(s),
      * set field hint with this info.
      *
@@ -43,7 +67,10 @@ class Fieldset
      * @return \Magento\Framework\Data\Form\Element\AbstractElement
      */
     public function aroundAddField(OriginalFieldset $subject, callable $proceed, $elementId, $type, $config, $after = false, $isAdvanced = false) {
-        $path = $config['field_config']['path'] . '/' . $config['field_config']['id'];
+        $configPath = $config['field_config']['id'];
+        $groupPath = $config['field_config']['path'];
+        $path = $groupPath . '/' . $configPath;
+
         $scope = $config['scope'];
         $scopeId = $config['scope_id'];
         $section = $this->request->getParam('section'); //@todo: don't talk to request directly
@@ -53,6 +80,17 @@ class Fieldset
             $scope,
             $scopeId
         );
+
+        if(empty($overriddenLevels) && isset($config['name'])) {
+            // Accommodate certain false negatives where the config path is different than the field's ID.
+
+            $path = $groupPath . '/' . $this->getConfigPathFromName($config['name']);
+            $overriddenLevels = $this->helper->getOverriddenLevels(
+                $path,
+                $scope,
+                $scopeId
+            );
+        }
 
         if(!empty($overriddenLevels)) {
             $config['comment'] .= $this->helper->formatOverriddenScopes($section, $overriddenLevels);
